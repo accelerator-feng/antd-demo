dab4fadef25e2a0658df1b60d78b1e1c8a6f06a7
YinFeng <jddj123@163.com>
2017-05-06 10:03:32 +0800

    demo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+/*
 import { Router, Route, hashHistory } from 'react-router';
 import MediaQuery from 'react-responsive';
 import 'antd/dist/antd.css';
@@ -27,3 +28,50 @@ ReactDOM.render(
   <Index/>,
   document.getElementById('root')
 );
+*/
+const scaleNames = {
+  c: 'Celsius',
+  f: 'Fahrenheit'
+};
+
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {temperature: ''};
+  }
+
+  handleChange(e) {
+    this.setState({temperature: e.target.value});
+  }
+
+  render() {
+    const temperature = this.state.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
+        <input value={temperature}
+               onChange={this.handleChange} />
+
+      </fieldset>
+    );
+  }
+}
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div>
+        <TemperatureInput scale="c" />
+        <TemperatureInput scale="f" />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Calculator />,
+  document.getElementById('root')
+);
+
+
